2b03a02b39e54019b7ce29ccdc7c4c161f050c1c
Cristian Ramirez <cristianramirezgt@gmail.com>
2017-07-19 17:23:51 -0600

    Metodos modificados y corregidos problemas con el procedimiento almacenad

--- ControlObraAPI/Models/ControlObraModel.Context.cs
+++ ControlObraAPI/Models/ControlObraModel.Context.cs
@@ -52,5 +52,55 @@ namespace ControlObraAPI.Models
     
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<VISTA_PROYECTO_MODELO_OBRA_ACTIVIDAD_TAREA_DETALLE_Result>("VISTA_PROYECTO_MODELO_OBRA_ACTIVIDAD_TAREA_DETALLE", tIPO_CONSULTAParameter, pARAMETRO1Parameter, pARAMETRO2Parameter, pARAMETRO3Parameter, pARAMETRO4Parameter);
         }
+    
+        public virtual ObjectResult<VISTA_PROYECTO_MODELO_OBRA_ACTIVIDAD_TAREA_DETALLE1_Result> VISTA_PROYECTO_MODELO_OBRA_ACTIVIDAD_TAREA_DETALLE1(Nullable<int> tIPO_CONSULTA, Nullable<int> pARAMETRO1, Nullable<int> pARAMETRO2, Nullable<int> pARAMETRO3, Nullable<int> pARAMETRO4)
+        {
+            var tIPO_CONSULTAParameter = tIPO_CONSULTA.HasValue ?
+                new ObjectParameter("TIPO_CONSULTA", tIPO_CONSULTA) :
+                new ObjectParameter("TIPO_CONSULTA", typeof(int));
+    
+            var pARAMETRO1Parameter = pARAMETRO1.HasValue ?
+                new ObjectParameter("PARAMETRO1", pARAMETRO1) :
+                new ObjectParameter("PARAMETRO1", typeof(int));
+    
+            var pARAMETRO2Parameter = pARAMETRO2.HasValue ?
+                new ObjectParameter("PARAMETRO2", pARAMETRO2) :
+                new ObjectParameter("PARAMETRO2", typeof(int));
+    
+            var pARAMETRO3Parameter = pARAMETRO3.HasValue ?
+                new ObjectParameter("PARAMETRO3", pARAMETRO3) :
+                new ObjectParameter("PARAMETRO3", typeof(int));
+    
+            var pARAMETRO4Parameter = pARAMETRO4.HasValue ?
+                new ObjectParameter("PARAMETRO4", pARAMETRO4) :
+                new ObjectParameter("PARAMETRO4", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<VISTA_PROYECTO_MODELO_OBRA_ACTIVIDAD_TAREA_DETALLE1_Result>("VISTA_PROYECTO_MODELO_OBRA_ACTIVIDAD_TAREA_DETALLE1", tIPO_CONSULTAParameter, pARAMETRO1Parameter, pARAMETRO2Parameter, pARAMETRO3Parameter, pARAMETRO4Parameter);
+        }
+    
+        public virtual ObjectResult<VISTA_PROYECTO_MODELO_OBRA_ACTIVIDAD_TAREA_DETALLE2_Result> VISTA_PROYECTO_MODELO_OBRA_ACTIVIDAD_TAREA_DETALLE2(Nullable<int> tIPO_CONSULTA, Nullable<int> pARAMETRO1, Nullable<int> pARAMETRO2, Nullable<int> pARAMETRO3, Nullable<int> pARAMETRO4)
+        {
+            var tIPO_CONSULTAParameter = tIPO_CONSULTA.HasValue ?
+                new ObjectParameter("TIPO_CONSULTA", tIPO_CONSULTA) :
+                new ObjectParameter("TIPO_CONSULTA", typeof(int));
+    
+            var pARAMETRO1Parameter = pARAMETRO1.HasValue ?
+                new ObjectParameter("PARAMETRO1", pARAMETRO1) :
+                new ObjectParameter("PARAMETRO1", typeof(int));
+    
+            var pARAMETRO2Parameter = pARAMETRO2.HasValue ?
+                new ObjectParameter("PARAMETRO2", pARAMETRO2) :
+                new ObjectParameter("PARAMETRO2", typeof(int));
+    
+            var pARAMETRO3Parameter = pARAMETRO3.HasValue ?
+                new ObjectParameter("PARAMETRO3", pARAMETRO3) :
+                new ObjectParameter("PARAMETRO3", typeof(int));
+    
+            var pARAMETRO4Parameter = pARAMETRO4.HasValue ?
+                new ObjectParameter("PARAMETRO4", pARAMETRO4) :
+                new ObjectParameter("PARAMETRO4", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<VISTA_PROYECTO_MODELO_OBRA_ACTIVIDAD_TAREA_DETALLE2_Result>("VISTA_PROYECTO_MODELO_OBRA_ACTIVIDAD_TAREA_DETALLE2", tIPO_CONSULTAParameter, pARAMETRO1Parameter, pARAMETRO2Parameter, pARAMETRO3Parameter, pARAMETRO4Parameter);
+        }
     }
 }
